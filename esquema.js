302a1c42cefd1a522a9eaf8a5f3b92f85dea482d
Lucas Rocha <lucasbezerrar@gmail.com>
2024-04-07 18:07:53 -0300

    Add esquema

---
+++ esquema.js
@@ -0,0 +1,23 @@
+const cartProduct = {
+  product: {
+    status: temp,
+    id,
+    nome: 'teste01',
+    preco: 100,
+    imgPequena: 'url/teste-01.png',
+    cor: 'preto',
+    tamanho: 41,
+    quantidade: 1,
+  },
+  desconto: 0.1,
+  frete: 10,
+  total: ((pedido.preco * quantidade) + frete.value) * (1 - desconto),
+}
+
+
+const pedido = {
+  id,
+  numeroDoPedido: Number,
+  status: 'Finalizado',
+  nomeDoProduto: 'name'
+}
