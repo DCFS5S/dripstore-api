d90127572fc8f6c1418610fea96f16ea04a4f989
Flávia Reis <flaviarster@gmail.com>
2024-04-16 20:31:22 -0300

    Criei validation

---
+++ validations/newUserValidation.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+    name: Joi.string().required(),
+    cpf: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    phone: Joi.string(),
+    address: Joi.string(),
+    neighborhood: Joi.string(),
+    city: Joi.string(),
+    zip: Joi.string(),
+    addressComplement: Joi.string()
+});
+
+module.exports = {
+    userSchema
+};
