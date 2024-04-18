fa68502869c25d5e34484e2ca5266a23de70d201
Eduardo Lopes <montalverne.edu@gmail.com>
2024-04-18 20:11:52 -0300

    wip: Atualizei as rotas de users e criei a rota de auth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const productController = require('../controllers/productsController');
 const categoriesController = require('../controllers/categoriesController');
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
 // const authMiddleware = require('../middlewares/authMiddleware')
 
 
@@ -15,7 +13,5 @@ router.put('/products/:productId', productController.update)
 router.get('/categories', categoriesController.list)
 router.post('/categories', categoriesController.create)
 
-router.post('/register', userController.create)
-router.post('/login',authController.login)
 
 module.exports = router;
